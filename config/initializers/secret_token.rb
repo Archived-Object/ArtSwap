cacf0ff75a97bc65feaf74f13453625ce7dea8d9
PurpleTengu <mhuan13@gmail.com>
2012-02-08 20:08:38 -0500

    set up with default rails application, worked on getting heroku to exist

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '4a4cc0d87b285c1bc96f281b43ec474e89564fe8f16958bb030aadc2577865c93c0e353524dd4829799008f14c91099920538ef9d5d6111555122d65dda6905b'
